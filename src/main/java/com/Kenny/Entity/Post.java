25b9b38c5356020d171835d5f4152b87de6ba5db
Kenny Lam <kinwai.lam730@gmail.com>
2020-06-03 23:58:44 -0400

    Updated it with lombok

--- src/main/java/com/Kenny/Entity/Post.java
+++ src/main/java/com/Kenny/Entity/Post.java
@@ -1,49 +1,59 @@
 package com.Kenny.Entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@ToString
 public class Post {
+
     private int id;
     private String name;
     private String title;
     private String post;
 
-    public Post(int id, String name, String title, String post) {
-        this.id = id;
-        this.name = name;
-        this.title = title;
-        this.post = post;
-    }
-
-    public Post(){}
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public String getPost() {
-        return post;
-    }
-
-    public void setPost(String post) {
-        this.post = post;
-    }
+//    public Post(int id, String name, String title, String post) {
+//        this.id = id;
+//        this.name = name;
+//        this.title = title;
+//        this.post = post;
+//    }
+//
+//    public Post(){}
+//
+//    public int getId() {
+//        return id;
+//    }
+//
+//    public void setId(int id) {
+//        this.id = id;
+//    }
+//
+//    public String getName() {
+//        return name;
+//    }
+//
+//    public void setName(String name) {
+//        this.name = name;
+//    }
+//
+//    public String getTitle() {
+//        return title;
+//    }
+//
+//    public void setTitle(String title) {
+//        this.title = title;
+//    }
+//
+//    public String getPost() {
+//        return post;
+//    }
+//
+//    public void setPost(String post) {
+//        this.post = post;
+//    }
 }
